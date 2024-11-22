198dc3ad83e879e8f604c38ff592711319cced77
frigaaabdou23@gmail.com <frigaaabdou23@gmail.com>
2024-11-22 11:42:28 +0100

    V0.1.2

--- mern-estate/client/src/App.jsx
+++ mern-estate/client/src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './assets/pages/Home'
+import SignUp from './assets/pages/SignUp'
+import SignIn from './assets/pages/SignIn'
+import About from './assets/pages/About'
+import Profile from './assets/pages/Profile'
+import Header from './assets/components/Header'
+
+
+
 
 export default function App() {
   return (
-    <div className=' bg-red-400'>NETFLIX</div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/sign-in' element={<SignIn/>} />
+      <Route path='/sign-up' element={<SignUp/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/profile' element={<Profile/>} />
+    </Routes>
+    </BrowserRouter>
   )
-}
+} 
